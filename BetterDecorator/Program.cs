78a9b31fc60f07dba9ea0e769b420fd36beb935b
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-03-14 21:07:15 +0000

    Add summary

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    /// <summary>
+    /// Programa para decorar uma string.
+    /// </summary>
+    class Program
+    {
+        private static string Decor(string s, char dec)
+        {
+            return $"{dec}{dec}{dec} {s} {dec}{dec}{dec}";
+        }
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Decor("Good Morning!", '*'))
+            Console.WriteLine(Decor("Good Evening! ", '.'))
+        }
+    }
+}
