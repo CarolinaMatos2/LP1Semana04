b5d5f3c8715002081b504709732f4d9f044bcf17
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-03-15 09:50:40 +0000

    Verifica o valor de n

--- Special/Program.cs
+++ Special/Program.cs
@@ -7,8 +7,25 @@ namespace Special
         static void Main(string[] args)
         {
             Console.Write("Insert n-th number of the special sequence: ");
-            int n = int.Parse(Console.ReadLine()) 
+            int n = int.Parse(Console.ReadLine())
+            int result = GetSpecial(n);
+            Console.WriteLine(result);
 
+        }
+        private static int GetSpecial(int n)
+        {
+            if(n == 0)
+            {
+                return 0;
+            }
+            else if(n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return GetSpecial(n - 1)
+            }
         }
     }
 }
